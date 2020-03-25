f009cb408f7f62c16172780adea60ee9e268ec41
ZushiBot <62661424+ZushiBot@users.noreply.github.com>
2020-03-25 22:49:35 +0600

    Array_Linear_Search

---
+++ Array_Linear_Search.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+main()
+{
+    int v;
+    int n;
+    printf("how many number you wants to add ?");
+    scanf("%d",&n);
+    int a[n];
+    printf("Complete Array using Int values : \n");
+
+    for (size_t i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    printf("input a number \n");
+    scanf("%d",&v);
+
+
+int count=0;
+    for (size_t i=0;i<n;i++)
+    {
+        if (v==a[i])
+            {
+                printf("location is at a[%d]\n",i);
+                count++;
+                break;
+
+            }
+    }
+
+    if (count!=0)
+    {
+        printf("Found");
+    }
+    else
+    {
+        printf("not Found");
+    }
+
+}
