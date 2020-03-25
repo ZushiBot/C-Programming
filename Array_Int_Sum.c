43c5408205790c86b219d31860dfae164c5f051f
ZushiBot <62661424+ZushiBot@users.noreply.github.com>
2020-03-25 22:11:22 +0600

    Array_Int_Sum.c

---
+++ Array_Int_Sum.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+main()
+{
+
+
+  int n;
+  printf("how many numbers you gonna add ?\n");
+  scanf("%d",&n);
+
+  int a[n];
+  printf("Input Numbers : \n" );
+
+  for (size_t i=0;i<n;i++)
+  {
+    scanf("%d",&a[i] );
+  }
+
+  printf("Output : \n");
+  for (size_t i=0;i<n;i++)
+  {
+    printf("%d  ",a[i] );
+  }
+  printf("\n");
+
+
+
+  int sum=0;
+
+  for (size_t i=0;i<n;i++)
+  {
+    sum+=a[i];
+  }
+
+  printf("The Sum is = %d",sum);
