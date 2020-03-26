8b76dea7efdb8f3c7e03533dcb3131f2660c1d0a
ZushiBot <62661424+ZushiBot@users.noreply.github.com>
2020-03-26 19:30:58 +0600

    string_check_Plaindrome

---
+++ string_check_Plaindrome.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+main()
+{
+    char name1[30]={'\0'};
+    printf("Name One : ");
+    scanf("%s",&name1);
+
+    //done;
+    int i=0;
+    while (name1[i]!='\0')
+    {
+        i++;
+    }
+
+    char temp[30]={'\0'};
+    int j=0;
+    while (name1[j]!=0){
+    temp[i-1]=name1[j];
+    i--;
+    j++;
+    }
+
+    printf("Reversed : %s ",temp);
+
+    int cmp = strcmp(temp,name1);
+
+    if(cmp==0)
+    {
+        printf("\nPalindrom");
+    }
+    else
+        printf("\nNot Palindrome");
+
+}
