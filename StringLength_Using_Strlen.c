0c870ba95c6e69ce3307886f5e3aa0065f883856
ZushiBot <62661424+ZushiBot@users.noreply.github.com>
2020-03-26 15:03:44 +0600

    StringLength_Using_Strlen

---
+++ StringLength_Using_Strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main()
+
+{
+    char name[30];
+    printf("Input Your Name :\n");
+    scanf("%s",&name);
+
+    int i=0;
+    while (name[i]!='\0')
+    {
+        printf("%c\n",name[i]);
+        i++;
+    }
+    printf("String Length %d",strlen(name));
+
+}
